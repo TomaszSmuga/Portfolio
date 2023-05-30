6472c959335cc090e0612f06eeed3d1d9cd5ed4d
Tomek <smuga2505tomek@gmail.com>
2023-05-30 10:57:48 +0200

    Added GlobalStyles styled-components, react-router

---
+++ Portfolio/src/components/MainNavMenu/MainNavMenu.styled.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.nav`
+  box-sizing: border-box;
+  display: flex;
+  width: 100%;
+  background-color: #eaeaea;
+  height: 15vh;
+  margin: 0;
+  padding: 0%;
+`;
+
+export const ItemBox = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ Portfolio/src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import { Wrapper, ItemBox } from "../components/MainNavMenu/MainNavMenu.styled";
+
+export const LandingPage = () => {
+  return (
+    <>
+      <Wrapper>
+        <ItemBox>Dupa</ItemBox>
+      </Wrapper>
+    </>
+  );
+};
