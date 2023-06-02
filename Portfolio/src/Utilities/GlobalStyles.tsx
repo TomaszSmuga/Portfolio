ddc40423c1b277aff2d3df0fdd63bdb8892fd6a4
Tomek <smuga2505tomek@gmail.com>
2023-06-02 19:25:37 +0200

    Made it work

--- Portfolio/src/Utilities/GlobalStyles.tsx
+++ Portfolio/src/Utilities/GlobalStyles.tsx
@@ -1,64 +1,64 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-  *,
-  *::before,
-  *::after {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+    /* *,
+    *::before,
+    *::after {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+    }
 
-  body {
-    font-family: 'Roboto', sans-serif;
-    background-color: #f6f6f6;
-  }
+    body {
+      font-family: 'Roboto', sans-serif;
+      background-color: #f6f6f6;
+    }
 
-  body, p, a {
-    color: #333;
-    text-decoration: none;
-  }
+    body, p, a {
+      color: #333;
+      text-decoration: none;
+    }
 
-  img {
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  }
+    img {
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
 
-  h1 {
-    font-size: 2rem;
-    font-weight: bold;
-    margin-bottom: 1rem;
-  }
+    h1 {
+      font-size: 2rem;
+      font-weight: bold;
+      margin-bottom: 1rem;
+    }
 
-  h2 {
-    font-size: 1.5rem;
-    font-weight: bold;
-    margin-bottom: 0.75rem;
-  }
+    h2 {
+      font-size: 1.5rem;
+      font-weight: bold;
+      margin-bottom: 0.75rem;
+    }
 
-  section {
-    padding: 2rem;
-  }
+    section {
+      padding: 2rem;
+    }
 
-  nav {
-    background-color: #333;
-    padding: 1rem;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    color: #fff;
-  }
+    nav {
+      background-color: #333;
+      padding: 1rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      color: #fff;
+    }
 
-  nav ul {
-    list-style-type: none;
-    display: flex;
-    gap: 1rem;
-  }
+    nav ul {
+      list-style-type: none;
+      display: flex;
+      gap: 1rem;
+    }
 
-  nav li {
-    cursor: pointer;
-  }
+    nav li {
+      cursor: pointer;
+    }
 
-  nav a {
-    color: #fff;
-  }
-`;
+    nav a {
+      color: #fff;
+    }
+  `; */
