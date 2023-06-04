ef2ab510c82757269d00e38e6232e6b3cbbf76a9
Tomek <smuga2505tomek@gmail.com>
2023-06-04 20:17:32 +0200

    deleted global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import { useState } from "react";
 import "./App.css";
-// import { FormComponent } from "./pages/LandingPage";
-import { GlobalStyles } from "./Utilities/GlobalStyles";
-import { Task } from "./pages/Task";
 import { Test } from "./pages/TestPage";
-import { TestMap2 } from "./pages/LandingPage";
 
 const App: React.FC = () => {
   return (
     <>
-      {/* <GlobalStyles /> */}
-      {/* <TestMap2 /> */}
       <Test />
     </>
   );
