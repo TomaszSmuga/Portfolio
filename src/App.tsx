5272fb226a07b5fb6371e3d5495b28bdc010ebbd
Tomek <smuga2505tomek@gmail.com>
2023-06-15 11:28:04 +0200

    Stayed with the repetetive code for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import { Test } from "./pages/TestPage";
-import Test2 from "./pages/TestPage2";
 
 const App: React.FC = () => {
   return (
     <>
-      {/* <Test /> */}
-      <Test2 />
+      <Test />
     </>
   );
 };
