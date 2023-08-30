27ed9a47c445f18f81e80be0aeed7aaa56040e10
Tomasz Smuga <112390369+TomaszSmuga@users.noreply.github.com>
2023-08-30 15:14:29 +0200

    Changed api

--- src/Utilities/api.ts
+++ src/Utilities/api.ts
@@ -1,2 +1,2 @@
 export const GOOGLE_SHEET_API_LINK =
-  "https://sheet.best/api/sheets/cc6435f2-f130-4620-97b9-ef42ac0996e8";
+  "https://sheet.best/api/sheets/5b90be46-8889-4300-ae49-02652c005b9f";
